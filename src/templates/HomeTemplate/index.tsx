2fa958b8f9487856563a03d416cb6b5a50cbbd93
Maskalenkas <maskalenkasdoc@gmail.com>
2022-05-23 08:52:03 -0300

    feat: Componente Container incluido na home

--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -1,10 +1,13 @@
+import Container from 'components/atoms/Container'
 import Hero from 'components/molecules/Hero'
 import React from 'react'
 import style from './styles.module.scss'
 
 const HomeTemplate = () => (
   <main data-component-home-template className={style.wrapper}>
-    <Hero />
+    <Container background="secondary">
+      <Hero />
+    </Container>
   </main>
 )
 
