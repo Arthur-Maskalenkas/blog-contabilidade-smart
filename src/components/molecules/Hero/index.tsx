a5d6e525ce7703dbe50e47f9a705757088139d9f
Arthur Maskalenkas <arthur.lins@penseavanti.com.br>
2022-05-26 03:21:07 -0300

    styles: Componente Hero estilizado

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -4,33 +4,34 @@ import Button from 'components/atoms/Button'
 import Image from 'next/image'
 
 const Hero = () => (
-  <div data-component-hero className={styles.wrapper}>
-    <h1 className={styles.title}>Contabilidade exclusiva para médicos</h1>
-    <p className={styles.description}>
-      Abertura da PJ grátis e contabilidade mensal especializada em médicos.
-      Cuidamos de tudo para você. Gaste seu tempo no que importa: seus pacientes
-      e sua carreira.
-    </p>
-
-    <div>
-      <Button size="large" variant="primary">
-        Quero abrir a minha conta pj
-      </Button>
-      <Button size="large" variant="primary" outline>
-        Quero trocar de contador
-      </Button>
-
-      <div className={styles.imgContainer}>
-        <Image
-          src="https://alume.com/wp-content/uploads/2022/04/contabilidade-hero.png"
-          alt="Um medico olhando para o monitor do computador"
-          layout="responsive"
-          width="100%"
-          height="100%"
-        />
+  <section data-component-hero className={styles.wrapper}>
+    <div className={styles.leftSide}>
+      <h1 className={styles.title}>Contabilidade exclusiva para médicos</h1>
+      <p className={styles.description}>
+        Abertura da PJ grátis e contabilidade mensal especializada em médicos.
+        Cuidamos de tudo para você. Gaste seu tempo no que importa: seus
+        pacientes e sua carreira.
+      </p>
+      <div className={styles.containerButtons}>
+        <Button size="full" variant="primary">
+          Quero abrir a minha conta pj
+        </Button>
+        <Button size="full" variant="primary" outline>
+          Quero trocar de contador
+        </Button>
       </div>
     </div>
-  </div>
+
+    <div className={styles.imgContainer}>
+      <Image
+        src="https://alume.com/wp-content/uploads/2022/04/contabilidade-hero.png"
+        alt="Um medico olhando para o monitor do computador"
+        layout="responsive"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  </section>
 )
 
 export default Hero
