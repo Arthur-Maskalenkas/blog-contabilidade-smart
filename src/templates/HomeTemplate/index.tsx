d8a6b0d70d99ee8dbc99380ab26174d668c91143
Maskalenkas <maskalenkasdoc@gmail.com>
2022-05-28 21:08:23 -0300

    feat: Componente OurPlans implementado no template da Home

--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -4,6 +4,7 @@ import Hero from 'components/molecules/Hero'
 import ContainerDeals from 'components/organisms/ContainerDeals'
 import React from 'react'
 import style from './styles.module.scss'
+import OurPlans from 'components/molecules/OurPlans'
 
 const HomeTemplate = () => (
   <main data-component-home-template={true} className={style.wrapper}>
@@ -12,8 +13,12 @@ const HomeTemplate = () => (
     </Container>
 
     <Container background={'primary'}>
-      <Title>Por que escolher a Alume?</Title>
+      <Title variation={'ourSection'}>Por que escolher a Alume?</Title>
       <ContainerDeals />
+    </Container>
+
+    <Container background={'secondary'}>
+      <OurPlans />
     </Container>
   </main>
 )
