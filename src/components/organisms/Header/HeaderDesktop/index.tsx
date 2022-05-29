c2fbbf321dd33fd46f8a7fff91aaaade91e654f3
Maskalenkas <maskalenkasdoc@gmail.com>
2022-05-29 18:27:47 -0300

    refactor: Componente Menu Header acompanhando a tela

--- src/components/organisms/Header/HeaderDesktop/index.tsx
+++ src/components/organisms/Header/HeaderDesktop/index.tsx
@@ -1,21 +1,45 @@
 import styles from 'components/organisms/Header/HeaderDesktop/styles.module.scss'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from 'components/atoms/Logo'
 import { listOfMenuLinks } from 'helpers/data/links-menu'
 import Links from 'components/atoms/Links'
 
-const HeaderDesktop = () => (
-  <div className={styles.wrapper}>
-    <Logo isMobile={false} />
+const HeaderDesktop = () => {
+  const [menuScrolling, setMenuScrolling] = useState<boolean>(false)
 
-    <nav>
-      {listOfMenuLinks.map((link, index) => (
-        <Links href={link.href} key={index} variant="header-desktop">
-          {link.label}
-        </Links>
-      ))}
-    </nav>
-  </div>
-)
+  const changeMenuAppearance = () => {
+    if (window.scrollY > 100) {
+      setMenuScrolling(true)
+
+      return
+    }
+
+    setMenuScrolling(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeMenuAppearance)
+
+    return () => window.removeEventListener('scroll', changeMenuAppearance)
+  }, [])
+
+  const classNameIfMenuScrolling = menuScrolling ? styles.menuScrolling : ''
+
+  return (
+    <div className={`${styles.wrapper} ${classNameIfMenuScrolling}`}>
+      <div className={`${styles.contentWrapper}`}>
+        <Logo isMobile={false} />
+
+        <nav>
+          {listOfMenuLinks.map((link, index) => (
+            <Links href={link.href} key={index} variant="header-desktop">
+              {link.label}
+            </Links>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
 
 export default HeaderDesktop
