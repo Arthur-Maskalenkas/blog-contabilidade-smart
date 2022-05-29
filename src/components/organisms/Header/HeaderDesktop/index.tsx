2a0d58602cc4b0ffbc66cc9c577e6c93de4dcae7
Maskalenkas <maskalenkasdoc@gmail.com>
2022-05-29 20:21:40 -0300

    refactor: Animações adicionadas ao componente Header

--- src/components/organisms/Header/HeaderDesktop/index.tsx
+++ src/components/organisms/Header/HeaderDesktop/index.tsx
@@ -4,17 +4,41 @@ import Logo from 'components/atoms/Logo'
 import { listOfMenuLinks } from 'helpers/data/links-menu'
 import Links from 'components/atoms/Links'
 
+interface menuScrolling {
+  isScrolling: boolean
+  wasScrolled: boolean
+  position: number
+}
+
 const HeaderDesktop = () => {
-  const [menuScrolling, setMenuScrolling] = useState<boolean>(false)
+  const distanceToMenuScrollWork = 200
+  const distanceToMenuScrollAppear = 900
+
+  const [menuScrolling, setMenuScrolling] = useState<menuScrolling>({
+    isScrolling: false,
+    position: 0,
+    wasScrolled: false
+  })
 
   const changeMenuAppearance = () => {
-    if (window.scrollY > 100) {
-      setMenuScrolling(true)
+    if (window.scrollY > distanceToMenuScrollAppear) {
+      setMenuScrolling((prevState) => ({
+        ...prevState,
+        isScrolling: true,
+        wasScrolled: true
+      }))
+    }
+
+    if (window.scrollY > distanceToMenuScrollWork) {
+      setMenuScrolling((prevState) => ({
+        ...prevState,
+        position: window.scrollY
+      }))
 
       return
     }
 
-    setMenuScrolling(false)
+    setMenuScrolling((v) => ({ ...v, isScrolling: false, wasScrolled: false }))
   }
 
   useEffect(() => {
@@ -23,20 +47,38 @@ const HeaderDesktop = () => {
     return () => window.removeEventListener('scroll', changeMenuAppearance)
   }, [])
 
-  const classNameIfMenuScrolling = menuScrolling ? styles.menuScrolling : ''
+  const classNameIfMenuScrolling = menuScrolling.isScrolling
+    ? styles.menuScrolling
+    : ''
+
+  const classNameIfMenuWasScrolled = menuScrolling.wasScrolled
+    ? styles.menuWasScrolled
+    : ''
+
+  const classNameWithPosition =
+    menuScrolling.position > distanceToMenuScrollAppear
+      ? styles.menuPositionDown
+      : styles.menuPositionMiddle
 
   return (
-    <div className={`${styles.wrapper} ${classNameIfMenuScrolling}`}>
-      <div className={`${styles.contentWrapper}`}>
-        <Logo isMobile={false} />
+    <div className={styles.wrapper}>
+      {menuScrolling.isScrolling && (
+        <div className={styles.containerToNotAffectScroll} />
+      )}
+      <div
+        className={`${styles.menuWrapper} ${classNameIfMenuScrolling} ${classNameWithPosition} ${classNameIfMenuWasScrolled}`}
+      >
+        <div className={`${styles.contentWrapper}`}>
+          <Logo isMobile={false} />
 
-        <nav>
-          {listOfMenuLinks.map((link, index) => (
-            <Links href={link.href} key={index} variant="header-desktop">
-              {link.label}
-            </Links>
-          ))}
-        </nav>
+          <nav>
+            {listOfMenuLinks.map((link, index) => (
+              <Links href={link.href} key={index} variant="header-desktop">
+                {link.label}
+              </Links>
+            ))}
+          </nav>
+        </div>
       </div>
     </div>
   )
